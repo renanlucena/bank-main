d425bb0dcd40cbecb4c0bf903d807fece14da0f0
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-07 17:08:31 -0300

    capturando uma foto e finalizando a criação de conta

--- js/camera.js
+++ js/camera.js
@@ -1,7 +1,11 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
-
+const botaoTirarFoto = document.querySelector("[data-tirar-foto");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+const botaoEnviarFoto = document.querySelector("[data-enviar]");
+let imagemURL = "";
 
 botaoIniciarCamera.addEventListener("click", async function (){
     const iniciarVideo = await navigator.mediaDevices
@@ -13,3 +17,27 @@ botaoIniciarCamera.addEventListener("click", async function (){
     video.srcObject = iniciarVideo;
 
 });
+
+botaoTirarFoto.addEventListener("click", function(){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.heght);
+
+    imagemURL = canvas.toDataURL("image/jpeg");
+
+    campoCamera.style.display = "none";
+
+    mensagem.style.display = "block";
+
+})
+
+    
+botaoEnviarFoto.addEventListener("click", () =>{
+    const receberDados = localStorage.getItem("cadastro");
+    const converteRetorno = JSON.parse(receberDados);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+
+    window.location.href = "./abrir-conta-form-3.html";
+
+})
